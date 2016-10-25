9a71de16df4ba4e440bfbedb5d67d01ef52e7a13
Emiapwil (Gao Kai) <emiapwil@gmail.com>
2016-10-25 10:42:20 -0700

    Render the script a bit to avoid duplicates
    Signed-off-by: Emiapwil (Gao Kai) <emiapwil@gmail.com>

--- ofstabilizer.py
+++ ofstabilizer.py
@@ -6,8 +6,9 @@ import sys
 import time
 
 URL_PREFIX = "http://{}:8181/restconf/{}"
-INVENTORY = "/opendaylight-inventory:nodes"
-FLOW = INVENTORY + "/node/{}/flow-node-inventory:table/{}/flow/{}"
+INVENTORY_URL = URL_PREFIX + "/opendaylight-inventory:nodes"
+TABLE_URL = INVENTORY_URL + "/node/{}/flow-node-inventory:table/{}"
+FLOW_URL = TABLE_URL + "/flow/{}"
 OP = "operational"
 CFG = "config"
 TABLE_ID = 0
@@ -53,29 +54,63 @@ FLOW_RULE_TEMPLATE='''
 }
 '''
 
+def is_matching_ethernet_protocol(flow, protocol):
+    try:
+        match = flow['match']
+        if len(match) > 1:
+            return False
+        ethernet_match = match['ethernet-match']
+        if len(ethernet_match) > 1:
+            return False
+        return ethernet_match['ethernet-type']['type'] == protocol
+    except Exception:
+        return False
+
+def is_sending_to_controller(flow):
+    try:
+        instructions = flow['instructions']['instruction']
+        if len(instructions) > 1:
+            return False
+        actions = instructions[0]['apply-actions']['action']
+        if len(actions) > 1:
+            return False
+        return actions[0]['output-action']['output-node-connector'].upper() == 'CONTROLLER'
+    except Exception:
+        return False
+
 def to_hex(value):
     return '0x{:04x}'.format(value)
 
 def check_and_install(controller_ip, node_id, proto):
-    pattern = URL_PREFIX + FLOW
+    tbl_url = TABLE_URL.format(controller_ip, OP, node_id, TABLE_ID)
 
-    op_url = pattern.format(controller_ip, OP, node_id, TABLE_ID, to_hex(proto))
-    cfg_url = pattern.format(controller_ip, CFG, node_id, TABLE_ID, to_hex(proto))
+    response = requests.get(tbl_url, headers=HEADERS, auth=AUTH)
+    checked = False
+    try:
+        table = response.json()['flow-node-inventory:table'][0]
+        for flow in table['flow']:
+            if not is_matching_ethernet_protocol(flow, proto):
+                continue
+            if not is_sending_to_controller(flow):
+                continue
+            checked = True
+            break;
+    except Exception:
+        pass
 
-    print(op_url)
-    print(cfg_url)
+    print('{} status: {}'.format(node_id, checked))
 
-    response = requests.get(op_url, headers=HEADERS, auth=AUTH)
-    if not response.ok:
+    if not checked:
+        fl_url = FLOW_URL.format(controller_ip, CFG, node_id, TABLE_ID, to_hex(proto))
         priority = 100 if proto == LLDP else 1
         data = FLOW_RULE_TEMPLATE % ('\"{}\"'.format(to_hex(proto)), proto, priority)
-        response = requests.put(cfg_url, headers=HEADERS, data=data, auth=AUTH)
+        response = requests.put(fl_url, headers=HEADERS, data=data, auth=AUTH)
         print(response.status_code)
 
 def stabilize(controller_ip):
     try:
         while True:
-            pattern = URL_PREFIX + INVENTORY
+            pattern = INVENTORY_URL
             url = pattern.format(controller_ip, OP)
 
             response = requests.get(url, headers=HEADERS, auth=AUTH)
